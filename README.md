bf1a56ff120293e617c1d9129da36a69f95113a7
w84miracle <w84miracle@gmail.com>
2016-05-30 17:21:24 +0800

    clean repo

---
+++ README.md
@@ -0,0 +1,16 @@
+# Flask-SB-Admin2
+SB Admin v2.0 template for python framework flask
+Installation
+------------
+```
+git clone https://github.com/w84miracle/flask-sb-admin2.git
+cd flask-sb-admin2/
+./sbadmin.py
+```
+visit http://127.0.0.1:5000/
+
+Documents
+------------
+https://github.com/mitsuhiko/flask
+
+https://github.com/BlackrockDigital/startbootstrap-sb-admin-2
---
+++ sbadmin.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from flask import Flask, url_for, render_template, send_from_directory
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<pagename>')
+def admin(pagename):
+    return render_template(pagename+'.html')
+
+@app.route('/<path:resource>')
+def serveStaticResource(resource):
+	return send_from_directory('static/', resource)
+
+@app.route("/test")
+def test():
+    return "<strong>It's Alive!</strong>"
+
+if __name__ == '__main__':
+    app.run()
